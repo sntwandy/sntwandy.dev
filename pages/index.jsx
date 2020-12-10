5f0b60af3b1c86f1840b8523015dccf416a1e911
Wandy Santana <sntwandy@gmail.com>
2020-12-10 01:48:09 -0400

    Building message

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import styled, {createGlobalStyle} from 'styled-components';
 
 const Index = () => (
-  <h1>sntwandy.dev</h1>
+  <Container>
+    <Title>sntwandy.dev is building...🔨🔨</Title>
+    <Subtitle>Will be ready very soon.</Subtitle>
+    <div className="lds-heart"><div></div></div>
+  </Container>
 )
+
+const Title = styled.h1`
+  font-size: 25px;
+  font-weight: 300;
+`
+
+const Container = styled.div`
+  display: flex;
+  align-items:center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`
+
+const Subtitle = styled.h3`
+  font-size: 18px;
+  font-weight: 400;
+`
 
 export default Index;
